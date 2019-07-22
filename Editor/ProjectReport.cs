ea03eccac26f4aa961e90c39d19e47cb42061a93
Marco Trivellato <marcot@unity3d.com>
2019-07-22 11:43:38 +0100

    search for api calls in all project assemblies for the player

--- Editor/ProjectReport.cs
+++ Editor/ProjectReport.cs
@@ -2,11 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
 using Mono.Cecil;
 using Mono.Cecil.Cil;
 using UnityEditor;
+using UnityEditor.Compilation;
 using UnityEngine;
 using UnityEditor.Macros;
 
@@ -14,6 +13,8 @@ namespace Editor
 {
     public class ProjectReport
     {
+        private Assembly[] m_PlayerAssemblies;
+        
         private DefinitionDatabase m_ApiCalls;
         private DefinitionDatabase m_ProjectSettings;
 
@@ -23,85 +24,94 @@ namespace Editor
         {
             m_ApiCalls = new DefinitionDatabase("ApiDatabase");
             m_ProjectSettings = new DefinitionDatabase("ProjectSettings");
+            
+            m_PlayerAssemblies = CompilationPipeline.GetAssemblies(AssembliesType.Player);
         }
         
         public void Create()
         {
+            m_ProjectIssues.Clear();
             AnalyzeApiCalls(m_ApiCalls.m_Definitions);
             AnalyzeProjectSettings(m_ProjectSettings.m_Definitions);
         }                
 
         public void AnalyzeApiCalls(List<ProblemDefinition> problemDefinitions)
         {
-            string assemblyPath = "Library/ScriptAssemblies/Assembly-CSharp.dll";
-            if (!File.Exists(assemblyPath))
-            {
-                Debug.LogError("Assembly-CSharp.dll not found.");
-                return;
-            }
+            Debug.Log("Analyzing Scripts...");
 
-            Debug.Log("Analyzing project...");
-            using (var a = AssemblyDefinition.ReadAssembly(assemblyPath, new ReaderParameters() { ReadSymbols = true} ))
+            // Analyse all Player assemblies, including Package assemblies.
+            foreach (var playerAssembly in m_PlayerAssemblies)
             {
-    //            var callInstructions = a.MainModule.Types.SelectMany(t => t.Methods)
-    //                .Where(m => m.HasBody)
-    //                .SelectMany(m => m.Body.Instructions)
-    //                .Where(i => i.OpCode == Mono.Cecil.Cil.OpCodes.Call);
-    //
-    //            var myProblems = problemDefinitions
-    //                .Where(problem =>
-    //                    callInstructions.Any(ci => ((MethodReference) ci.Operand).DeclaringType.Name == problem.type.Name))
-    //                .Select(p => new Issue {def = p});
-    //
-    //            issues.AddRange(myProblems);
-
-                foreach (var m in a.MainModule.Types.SelectMany(t => t.Methods))
+                string assemblyPath = playerAssembly.outputPath;
+                if (!File.Exists(assemblyPath))
                 {
-                    if (!m.HasBody)
-                        continue;
-            
-                    foreach (var inst in m.Body.Instructions.Where(i => i.OpCode == OpCodes.Call))
+                    Debug.LogError($"{assemblyPath} not found.");
+                    return;
+                }
+    
+                using (var a = AssemblyDefinition.ReadAssembly(assemblyPath, new ReaderParameters() { ReadSymbols = true} ))
+                {
+        //            var callInstructions = a.MainModule.Types.SelectMany(t => t.Methods)
+        //                .Where(m => m.HasBody)
+        //                .SelectMany(m => m.Body.Instructions)
+        //                .Where(i => i.OpCode == Mono.Cecil.Cil.OpCodes.Call);
+        //
+        //            var myProblems = problemDefinitions
+        //                .Where(problem =>
+        //                    callInstructions.Any(ci => ((MethodReference) ci.Operand).DeclaringType.Name == problem.type.Name))
+        //                .Select(p => new Issue {def = p});
+        //
+        //            issues.AddRange(myProblems);
+    
+                    foreach (var m in a.MainModule.Types.SelectMany(t => t.Methods))
                     {
-                        var calledMethod = ((MethodReference) inst.Operand);
-//                        var p = problemDefinitions.SingleOrDefault(c => c.type.Name == calledMethod.DeclaringType.Name);
-                        var p = problemDefinitions.SingleOrDefault(c => c.type == calledMethod.DeclaringType.Name);
-                        //if (p.type != null && m.HasCustomDebugInformations)
-                        if (p != null && m.DebugInformation.HasSequencePoints)
+                        if (!m.HasBody)
+                            continue;
+                
+                        foreach (var inst in m.Body.Instructions.Where(i => i.OpCode == OpCodes.Call))
                         {
-                            var msg = string.Empty;
-                            SequencePoint s = null;
-                            for (var i = inst; i != null; i = i.Previous)
+                            var calledMethod = ((MethodReference) inst.Operand);
+    //                        var p = problemDefinitions.SingleOrDefault(c => c.type.Name == calledMethod.DeclaringType.Name);
+                            var p = problemDefinitions.SingleOrDefault(c => c.type == calledMethod.DeclaringType.Name);
+                            //if (p.type != null && m.HasCustomDebugInformations)
+                            if (p != null && m.DebugInformation.HasSequencePoints)
                             {
-                                s = m.DebugInformation.GetSequencePoint(i);
+                                var msg = string.Empty;
+                                SequencePoint s = null;
+                                for (var i = inst; i != null; i = i.Previous)
+                                {
+                                    s = m.DebugInformation.GetSequencePoint(i);
+                                    if (s != null)
+                                    {
+                                        msg = i == inst ? " exactly" : "nearby";
+                                        break;
+                                    }
+                                }
+                
                                 if (s != null)
                                 {
-                                    msg = i == inst ? " exactly" : "nearby";
-                                    break;
+                                    m_ProjectIssues.Add(new ProjectIssue
+                                    {
+                                        category = "API Call",
+                                        def = p,
+                                        url = s.Document.Url,
+                                        line = s.StartLine,
+                                        column = s.StartColumn
+                                    });
                                 }
+                
                             }
-            
-                            if (s != null)
-                            {
-                                m_ProjectIssues.Add(new ProjectIssue
-                                {
-                                    category = "API Call",
-                                    def = p,
-                                    url = s.Document.Url,
-                                    line = s.StartLine,
-                                    column = s.StartColumn
-                                });
-                                //                                Debug.Log($"Issue is {msg} {s.Document.Url} ({s.StartLine},{s.StartColumn})");
-                            }
-            
                         }
                     }
-                }
+                }                
             }
+            
+
         }
 
-        void AnalyzeAssembly(List<ProblemDefinition> problemDefinitions, Assembly assembly)
+        void SearchAndEval(ProblemDefinition p, System.Reflection.Assembly[] assemblies)
         {
-            foreach (var p in problemDefinitions)
+            foreach (var assembly in assemblies)
             {
                 try
                 {
@@ -109,29 +119,36 @@ namespace Editor
                         p.type, "get_" + p.method, new System.Type[0]{}, new object[0]{});
 
                     if (value.ToString() == p.value)
+                    {
                         m_ProjectIssues.Add(new ProjectIssue
                         {
                             category = "ProjectSettings",
                             def = p,
                             url = "N/A"
                         });
+                        
+                        // stop iterating assemblies
+                        break;
+                    }
                 }
                 catch (Exception e)
                 {
                     // TODO
                 }
             }
+
             
         }
         
         public void AnalyzeProjectSettings(List<ProblemDefinition> problemDefinitions)
         {
-            string [] assemblyNames = new string[]{"UnityEditor.dll", "UnityEngine.dll"}; 
-            var assemblies = AppDomain.CurrentDomain.GetAssemblies().Where(x => assemblyNames.Contains(x.ManifestModule.Name));
-
-            foreach (var assembly in assemblies)
+            Debug.Log("Analyzing Project Settings...");
+//            string [] assemblyNames = new string[]{"UnityEditor.dll", "UnityEngine.dll", "UnityEditor.WebGL.Extensions.dll"}; 
+//            var assemblies = AppDomain.CurrentDomain.GetAssemblies().Where(x => assemblyNames.Contains(x.ManifestModule.Name));
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            foreach (var p in problemDefinitions)
             {
-                AnalyzeAssembly(problemDefinitions, assembly);
+                SearchAndEval(p, assemblies);
             }
         }
 
