b662f60a9f9bd76a0f37bcb4a83693724ab6b8d7
Marco Trivellato <marcot@unity3d.com>
2019-11-19 10:21:40 +0000

    cleanup

--- Editor/ProjectAuditorConfig.cs
+++ Editor/ProjectAuditorConfig.cs
@@ -11,21 +11,6 @@ namespace Unity.ProjectAuditor.Editor
         public bool enableFailBuildOnIssues = false;
         public List<Rule> rules = new List<Rule>();
 
-        public bool IsRuleAction(ProblemDescriptor descriptor, Rule.Action action)
-        {
-            var ruleAction = descriptor.action;
-            var projectRule = rules.Where(r => r.id == descriptor.id).FirstOrDefault();
-            if (projectRule != null)
-            {
-                if (projectRule.action != Rule.Action.Default)
-                    ruleAction = projectRule.action;
-            }
-            
-            if (action == ruleAction)
-                return true;
-            return false;
-        }
-
         public Rule.Action GetAction(ProblemDescriptor descriptor)
         {
             var projectRule = rules.Where(r => r.id == descriptor.id).FirstOrDefault();
