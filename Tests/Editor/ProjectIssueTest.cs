8b99532329ce6ba68d400f90c7197265c4d6a200
Marco Trivellato <marcot@unity3d.com>
2019-11-26 14:58:29 +0000

    tests

---
+++ Tests/Editor/ProjectIssueTest.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using Unity.ProjectAuditor.Editor;
+
+namespace UnityEditor.ProjectAuditor.EditorTests
+{
+	class ProjectIssueTest {
+
+		[Test]
+		public void UninitializedIssueTestPasses()
+		{
+			var uninitialised = new ProjectIssue();
+			Assert.AreEqual(string.Empty, uninitialised.filename);
+			Assert.AreEqual(string.Empty, uninitialised.relativePath);
+			Assert.AreEqual(string.Empty, uninitialised.callingMethod);
+			Assert.AreEqual(string.Empty, uninitialised.callingMethodName);
+		}
+	}	
+}
+
