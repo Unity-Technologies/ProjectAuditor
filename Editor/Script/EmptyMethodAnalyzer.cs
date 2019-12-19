dcbc32f779d06bdc76b30e35794f6f9f164faeb1
Marco Trivellato <marcot@unity3d.com>
2019-12-19 21:15:56 +0000

    Empty Method analyzer

---
+++ Editor/Script/EmptyMethodAnalyzer.cs
@@ -0,0 +1,52 @@
+using System.Collections.Generic;
+using Mono.Cecil.Cil;
+
+namespace Unity.ProjectAuditor.Editor
+{
+    [ScriptAnalyzer]
+    public class EmptyMethodAnalyzer : IInstructionAnalyzer
+    {
+        private static readonly ProblemDescriptor descriptor = new ProblemDescriptor
+        {
+            id = 10201,
+            description = "Empty Method",
+            type = string.Empty,
+            method = string.Empty,
+            area = "CPU",
+            problem = "If this is a MonoBehaviour class, any empty Awake/Start/Update/LateUpdate/FixedUpdate will be included in the build and executed anyway.",
+            solution = "Remove any empty MonoBehaviour methods."
+        };
+
+        public static ProblemDescriptor GetDescriptor()
+        {
+            return descriptor;
+        }
+        
+        private OpCode[] m_OpCodes = new[] {OpCodes.Ret};
+        
+        public EmptyMethodAnalyzer(ScriptAuditor auditor)
+        {
+            auditor.RegisterDescriptor(descriptor);
+        }
+        
+        public ProjectIssue Analyze(Instruction inst)
+        {
+            if (inst.Previous != null)
+                return null;
+
+            var calleeNode = new CallTreeNode(descriptor.description);
+            return new ProjectIssue
+            {
+                description = descriptor.description,
+                category = IssueCategory.ApiCalls,
+                descriptor = descriptor,
+                callTree = calleeNode
+            };
+        }
+
+        public IEnumerable<OpCode> GetOpCodes()
+        {
+            return m_OpCodes;
+        }
+    }
+}
---
+++ Tests/Editor/EmptyMethodTest.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using Unity.ProjectAuditor.Editor;
+
+namespace UnityEditor.ProjectAuditor.EditorTests
+{
+	class EmptyMethodTest
+	{
+		private ScriptResource m_ScriptResource;
+		
+		[SetUp]
+		public void SetUp()
+		{
+			 m_ScriptResource = new ScriptResource("MyClass.cs", "using UnityEngine; class MyClass : MonoBehaviour { void Update() { } }");
+		}
+
+		[TearDown]
+		public void TearDown()
+		{
+			m_ScriptResource.Delete();
+		}
+
+		[Test]
+		public void EmptyMethodIsFound()
+		{
+			var scriptIssues = ScriptIssueTestHelper.AnalyzeAndFindScriptIssues(m_ScriptResource.relativePath);
+			
+			Assert.AreEqual(1, scriptIssues.Count());
+
+			var issue = scriptIssues.FirstOrDefault();
+			
+			Assert.NotNull(issue);
+			Assert.NotNull(issue.descriptor);
+			
+			Assert.AreEqual(Rule.Action.Default, issue.descriptor.action);
+			Assert.AreEqual(EmptyMethodAnalyzer.GetDescriptor().id, issue.descriptor.id);
+			Assert.True(string.IsNullOrEmpty(issue.descriptor.type));
+			Assert.True(string.IsNullOrEmpty(issue.descriptor.method));
+			
+			Assert.True(issue.name.Equals("MyClass.Update"));
+			Assert.True(issue.filename.Equals(m_ScriptResource.scriptName));
+			Assert.True(issue.description.Equals(EmptyMethodAnalyzer.GetDescriptor().description));
+			Assert.True(issue.callingMethod.Equals("System.Void MyClass::Update()"));
+			Assert.AreEqual(1, issue.line);
+			Assert.AreEqual(IssueCategory.ApiCalls, issue.category);
+		}
+	}	
+}
+
