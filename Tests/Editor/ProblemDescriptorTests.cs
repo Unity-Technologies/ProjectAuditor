2d01cba608b6a1b5864da98fc88055a07cdc0924
Marco Trivellato <marcot@unity3d.com>
2020-04-28 19:50:00 +0100

    ProblemDescriptor test coverave

--- Tests/Editor/ProblemDescriptorTests.cs
+++ Tests/Editor/ProblemDescriptorTests.cs
@@ -12,5 +12,37 @@ namespace UnityEditor.ProjectAuditor.EditorTests
             var uninitialised = new ProblemDescriptor();
             Assert.Null(uninitialised.description);
         }
+
+        [Test]
+        public void ProblemDescriptorsAreEqual()
+        {
+            var a = new ProblemDescriptor
+            {
+                id = 102001
+            };
+            var b = new ProblemDescriptor
+            {
+                id = 102001
+            };
+
+            Assert.True(a.Equals(a));
+            Assert.True(a.Equals((object)a));
+            Assert.True(a.Equals(b));
+            Assert.True(a.Equals((object)b));
+            b = null;
+            Assert.False(a.Equals(b));
+            Assert.False(a.Equals((object)b));
+        }
+
+        [Test]
+        public void ProblemDescriptorHashIsId()
+        {
+            var p = new ProblemDescriptor
+            {
+                id = 102001
+            };
+
+            Assert.True(p.GetHashCode() == p.id);
+        }
     }
 }
