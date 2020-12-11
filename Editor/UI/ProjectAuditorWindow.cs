222b66d552a1d25d7f3bb75f1509ce8aa8c99ff2
Marco Trivellato <marcot@unity3d.com>
2020-12-11 15:23:36 +0000

    fix tooltip

--- Editor/UI/ProjectAuditorWindow.cs
+++ Editor/UI/ProjectAuditorWindow.cs
@@ -96,7 +96,7 @@ namespace Unity.ProjectAuditor.Editor.UI
                 {
                     new ColumnStyle
                     {
-                        Content = new GUIContent("Actual Variants", "Total Variants"),
+                        Content = new GUIContent("Actual Variants", "Number of variants in the build"),
                         Width = 80,
                         MinWidth = 80,
                         Format = PropertyFormat.Integer
