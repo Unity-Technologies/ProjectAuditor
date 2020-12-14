3ee7c9e2e4d0ceb5f3980cf04c3a6a744ae678e1
Marco Trivellato <marcot@unity3d.com>
2020-12-14 13:07:54 +0000

    fix Cecil Resolve warning when analyzing CodeGen assembly

--- Editor/CodeAnalysis/ComponentSystemAnalysis.cs
+++ Editor/CodeAnalysis/ComponentSystemAnalysis.cs
@@ -7,18 +7,24 @@ namespace Unity.ProjectAuditor.Editor.CodeAnalysis
 {
     static class ComponentSystemAnalysis
     {
+        static readonly int k_EntitiesModuleHashCode = "Unity.Entities.dll".GetHashCode();
         static readonly int[] k_ClassNameHashCodes = {"Unity.Entities.ComponentSystem".GetHashCode(), "Unity.Entities.JobComponentSystem".GetHashCode()};
+        static readonly int k_ILPostProcessorHashCode = "Unity.CompilationPipeline.Common.ILPostProcessing.ILPostProcessor".GetHashCode();
 
         static readonly string[] k_UpdateMethodNames = {"OnUpdate"};
 
         public static bool IsComponentSystem(TypeReference typeReference)
         {
+            // handle special case where Assembly will fail to be Resolved
+            if (typeReference.FullName.GetHashCode() == k_ILPostProcessorHashCode)
+                return false;
+
             try
             {
                 var typeDefinition = typeReference.Resolve();
 
                 if (k_ClassNameHashCodes.FirstOrDefault(i => i == typeDefinition.FullName.GetHashCode()) != 0 &&
-                    typeDefinition.Module.Name.Equals("Unity.Entities.dll"))
+                    typeDefinition.Module.Name.GetHashCode() == k_EntitiesModuleHashCode)
                     return true;
 
                 if (typeDefinition.BaseType != null)
--- Editor/CodeAnalysis/MonoBehaviourAnalysis.cs
+++ Editor/CodeAnalysis/MonoBehaviourAnalysis.cs
@@ -7,7 +7,9 @@ namespace Unity.ProjectAuditor.Editor.CodeAnalysis
 {
     static class MonoBehaviourAnalysis
     {
-        static readonly int k_ClassNameHashCode = "UnityEngine.MonoBehaviour".GetHashCode();
+        static readonly int k_CoreModuleHashCode = "UnityEngine.CoreModule.dll".GetHashCode();
+        static readonly int k_MonoBehaviourHashCode = "UnityEngine.MonoBehaviour".GetHashCode();
+        static readonly int k_ILPostProcessorHashCode = "Unity.CompilationPipeline.Common.ILPostProcessing.ILPostProcessor".GetHashCode();
 
         static readonly string[] k_MagicMethodNames =
         {"Awake", "Start", "OnEnable", "OnDisable", "Update", "LateUpdate", "OnEnable", "FixedUpdate"};
@@ -16,12 +18,16 @@ namespace Unity.ProjectAuditor.Editor.CodeAnalysis
 
         public static bool IsMonoBehaviour(TypeReference typeReference)
         {
+            // handle special case where Assembly will fail to be Resolved
+            if (typeReference.FullName.GetHashCode() == k_ILPostProcessorHashCode)
+                return false;
+
             try
             {
                 var typeDefinition = typeReference.Resolve();
 
-                if (typeDefinition.FullName.GetHashCode() == k_ClassNameHashCode &&
-                    typeDefinition.Module.Name.Equals("UnityEngine.CoreModule.dll"))
+                if (typeDefinition.FullName.GetHashCode() == k_MonoBehaviourHashCode &&
+                    typeDefinition.Module.Name.GetHashCode() == k_CoreModuleHashCode)
                     return true;
 
                 if (typeDefinition.BaseType != null)
