b290ccfe32019a7164af495a6275c464ae70a458
Marco Trivellato <marcot@unity3d.com>
2021-02-26 13:27:16 +0000

    review feedback: fix UnityEditor namespace

--- Editor/Auditors/SettingsAuditor.cs
+++ Editor/Auditors/SettingsAuditor.cs
@@ -37,8 +37,6 @@ namespace Unity.ProjectAuditor.Editor.Auditors
             m_ProjectSettingsMapping.Add(new KeyValuePair<string, string>("UnityEngine.Physics", "Project/Physics"));
             m_ProjectSettingsMapping.Add(
                 new KeyValuePair<string, string>("UnityEngine.Physics2D", "Project/Physics 2D"));
-            m_ProjectSettingsMapping.Add(new KeyValuePair<string, string>("UnityEngine.PlayerSettings",
-                "Project/Player"));
             m_ProjectSettingsMapping.Add(new KeyValuePair<string, string>("UnityEngine.Time", "Project/Time"));
             m_ProjectSettingsMapping.Add(new KeyValuePair<string, string>("UnityEngine.QualitySettings",
                 "Project/Quality"));
