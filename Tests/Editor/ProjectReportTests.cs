1d05a9305d2d538cc99d218bcf44df21af8cc482
Marco Trivellato <marcot@unity3d.com>
2021-03-01 11:07:23 +0000

    fix warning

--- Tests/Editor/ProjectReportTests.cs
+++ Tests/Editor/ProjectReportTests.cs
@@ -9,7 +9,9 @@ namespace UnityEditor.ProjectAuditor.EditorTests
 {
     class ProjectReportTests
     {
+#pragma warning disable 0414
         TempAsset m_TempAsset;
+#pragma warning restore 0414
 
         [OneTimeSetUp]
         public void SetUp()
