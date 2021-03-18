7df15cd437146beed14b4977d26109b950c059ba
Marco Trivellato <marcot@unity3d.com>
2021-03-18 15:06:36 +0000

    wip

--- Editor/SettingsAnalyzers/HDRenderPipelineAnalyzer.cs
+++ Editor/SettingsAnalyzers/HDRenderPipelineAnalyzer.cs
@@ -1,7 +1,10 @@
 ﻿
 using UnityEditor;
 using UnityEngine.Rendering.HighDefinition;
+using UnityEngine.SceneManagement;
 #if UNITY_2019_3_OR_NEWER
+using System;
+using UnityEngine;
 using UnityEngine.Rendering;
 using PackageInfo = UnityEditor.PackageManager.PackageInfo;
 #endif
@@ -10,8 +13,16 @@ namespace Unity.ProjectAuditor.Editor.SettingsAnalyzers
 {
     public class HDRenderPipelineAnalyzer : ISettingsAnalyzer
     {
-        static readonly ProblemDescriptor k_Descriptor = new ProblemDescriptor(
+        static readonly ProblemDescriptor k_LitShaderModeBoth = new ProblemDescriptor(
             202001,
+            "HDRP: Shader mode is set to Both",
+            string.Format("{0}|{1}", Area.BuildSize, Area.BuildTime),
+            "If shader mode is set to Both, shaders will be built for both Forward and Deferred rendering. This increases build time and size.",
+            "Change Shader mode to Forward or Deferred."
+        );
+
+        static readonly ProblemDescriptor k_LitShaderModeBothAndMixedCameras = new ProblemDescriptor(
+            202002,
             "HDRP: Shader mode is set to Both",
             string.Format("{0}|{1}", Area.BuildSize, Area.BuildTime),
             "If shader mode is set to Both, shaders will be built for both Forward and Deferred rendering. This increases build time and size.",
@@ -20,12 +31,13 @@ namespace Unity.ProjectAuditor.Editor.SettingsAnalyzers
 
         public void Initialize(IAuditor auditor)
         {
-            auditor.RegisterDescriptor(k_Descriptor);
+            auditor.RegisterDescriptor(k_LitShaderModeBoth);
+            auditor.RegisterDescriptor(k_LitShaderModeBothAndMixedCameras);
         }
 
         public int GetDescriptorId()
         {
-            return k_Descriptor.id;
+            return k_LitShaderModeBoth.id;
         }
 
         public ProjectIssue Analyze()
@@ -37,8 +49,35 @@ namespace Unity.ProjectAuditor.Editor.SettingsAnalyzers
                 var hdrpAsset = renderPipelineAsset as HDRenderPipelineAsset;
                 if (hdrpAsset != null)
                 {
-                    if (hdrpAsset.currentPlatformRenderPipelineSettings.supportedLitShaderMode == RenderPipelineSettings.SupportedLitShaderMode.Both)
-                        return new ProjectIssue(k_Descriptor, k_Descriptor.description, IssueCategory.ProjectSettings);
+                    if (hdrpAsset.currentPlatformRenderPipelineSettings.supportedLitShaderMode ==
+                        RenderPipelineSettings.SupportedLitShaderMode.Both)
+                    {
+                        var deferredCamera = false;
+                        var forwardCamera = false;
+                        for (int n = 0; n < SceneManager.sceneCount; ++n)
+                        {
+                            var scene = SceneManager.GetSceneAt(n);
+                            var roots = scene.GetRootGameObjects();
+                            foreach (var go in roots)
+                            {
+                                var renderingPath = go.GetComponent<Camera>().renderingPath;
+                                switch (renderingPath)
+                                {
+                                    case RenderingPath.DeferredLighting:
+                                        deferredCamera = true;
+                                        break;
+                                    case RenderingPath.Forward:
+                                        forwardCamera = true;
+                                        break;
+                                    default:
+                                        throw new Exception("Unexpected RenderingPath " + renderingPath);
+                                }
+                            }
+                        }
+                        if (deferredCamera && forwardCamera)
+                            return new ProjectIssue(k_LitShaderModeBothAndMixedCameras, k_LitShaderModeBothAndMixedCameras.description, IssueCategory.ProjectSettings);
+                        return new ProjectIssue(k_LitShaderModeBoth, k_LitShaderModeBoth.description, IssueCategory.ProjectSettings);
+                    }
                 }
             }
 #endif
