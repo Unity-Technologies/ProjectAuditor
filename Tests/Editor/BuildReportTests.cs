8f47730937f89bf2d4f9ffe32d2e97adcf6da2f7
Marco Trivellato <marcot@unity3d.com>
2021-04-06 16:38:39 +0100

    BuildReportIsFound test

--- Tests/Editor/BuildReportTests.cs
+++ Tests/Editor/BuildReportTests.cs
@@ -1,10 +1,29 @@
+using System.IO;
+using System.Linq;
 using NUnit.Framework;
+using Unity.ProjectAuditor.Editor;
 using Unity.ProjectAuditor.Editor.Auditors;
+using UnityEngine;
 
 namespace UnityEditor.ProjectAuditor.EditorTests
 {
     public class BuildReportTests
     {
+        private TempAsset m_TempAsset;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            var material = new Material(Shader.Find("UI/Default"));
+            m_TempAsset = TempAsset.Save(material, "Resources/Shiny.mat");
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            TempAsset.Cleanup();
+        }
+
         [Test]
         public void BuildReportIsSupported()
         {
@@ -16,6 +35,19 @@ namespace UnityEditor.ProjectAuditor.EditorTests
 #else
             Assert.False(isSupported);
 #endif
+        }
+
+        [Test]
+#if !UNITY_2019_4_OR_NEWER
+        [Ignore("Not Supported in this version of Unity")]
+#endif
+        public void BuildReportIsFound()
+        {
+            var issues = Utility.AnalyzeBuild().GetIssues(IssueCategory.BuildFiles);
+            var matchingIssue = issues.FirstOrDefault(i => i.relativePath.Equals(m_TempAsset.relativePath));
+
+            Assert.NotNull(matchingIssue);
+            Assert.True(matchingIssue.description.Equals(Path.GetFileNameWithoutExtension(m_TempAsset.relativePath)));
         }
     }
 }
