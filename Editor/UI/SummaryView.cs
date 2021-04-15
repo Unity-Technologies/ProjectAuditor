04406db36ef3361017d7d19dbf437abe39a403d3
Marco Trivellato <marcot@unity3d.com>
2021-04-15 09:30:14 +0100

    summary view tweaks

--- Editor/UI/SummaryView.cs
+++ Editor/UI/SummaryView.cs
@@ -15,10 +15,12 @@ namespace Unity.ProjectAuditor.Editor.UI
                 EditorGUILayout.LabelField("Analysis overview", EditorStyles.boldLabel);
 
                 EditorGUI.indentLevel++;
-                SummaryItem("Code Issues: " + s_Report.GetIssues(IssueCategory.Code).Length, IssueCategory.Code);
-                SummaryItem("Settings Issues: " + s_Report.GetIssues(IssueCategory.ProjectSettings).Length, IssueCategory.ProjectSettings);
-                SummaryItem("Assets in Resources folders: " + s_Report.GetIssues(IssueCategory.Assets).Length, IssueCategory.Assets);
-                SummaryItem("Shaders in the project: " + s_Report.GetIssues(IssueCategory.Shaders).Length, IssueCategory.Shaders);
+                SummaryItem("Code Issues:", s_Report.GetIssues(IssueCategory.Code).Length, IssueCategory.Code);
+                SummaryItem("Settings Issues:", s_Report.GetIssues(IssueCategory.ProjectSettings).Length, IssueCategory.ProjectSettings);
+                SummaryItem("Assets in Resources folders:", s_Report.GetIssues(IssueCategory.Assets).Length, IssueCategory.Assets);
+                SummaryItem("Shaders in the project:", s_Report.GetIssues(IssueCategory.Shaders).Length, IssueCategory.Shaders);
+                var buildAvailable = s_Report.GetIssues(IssueCategory.BuildFiles).Length > 0;
+                SummaryItem("Build Report available:", buildAvailable.ToString(), IssueCategory.BuildFiles);
                 EditorGUI.indentLevel--;
 
                 EditorGUILayout.Space();
@@ -27,10 +29,16 @@ namespace Unity.ProjectAuditor.Editor.UI
             EditorGUILayout.LabelField("Select a View from the toolbar to start browsing the report");
         }
 
-        static void SummaryItem(string text, IssueCategory category)
+        static void SummaryItem(string title, int value, IssueCategory category)
+        {
+            SummaryItem(title, value.ToString(), category);
+        }
+
+        static void SummaryItem(string title, string value, IssueCategory category)
         {
             EditorGUILayout.BeginHorizontal();
-            EditorGUILayout.LabelField(text);
+            EditorGUILayout.LabelField(title);
+            EditorGUILayout.LabelField(value, GUILayout.Width(60));
             if (GUILayout.Button("View", EditorStyles.miniButton))
                 OnChangeView(category);
             GUILayout.FlexibleSpace();
