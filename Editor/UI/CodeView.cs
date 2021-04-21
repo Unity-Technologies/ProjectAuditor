71b7e158b41720106a39dada81be509966cb9b51
Marco Trivellato <marcot@unity3d.com>
2021-04-21 13:41:18 +0100

    update CodeView

--- Editor/UI/CodeView.cs
+++ Editor/UI/CodeView.cs
@@ -8,17 +8,18 @@ namespace Unity.ProjectAuditor.Editor.UI
     {
         protected override void OnDrawInfo()
         {
+            EditorGUILayout.LabelField("- Use the Filters to reduce the number of reported issues");
+            EditorGUILayout.LabelField("- Use the Mute button to mark an issue as false-positive");
+
             if (NumCompilationErrors() > 0)
             {
+                EditorGUILayout.Space();
                 EditorGUILayout.BeginHorizontal();
                 EditorGUILayout.LabelField(Utility.ErrorIcon, GUILayout.MaxWidth(36));
-                EditorGUILayout.LabelField(new GUIContent("Code Analysis is incomplete due to compilation errors"));
+                EditorGUILayout.LabelField(new GUIContent("Code Analysis is incomplete due to compilation errors"), GUILayout.Width(330), GUILayout.ExpandWidth(false));
+                if (GUILayout.Button("View", EditorStyles.miniButton, GUILayout.Width(50)))
+                    OnChangeView(IssueCategory.CodeCompilerMessages);
                 EditorGUILayout.EndHorizontal();
-            }
-            else
-            {
-                EditorGUILayout.LabelField("- Use the Filters to reduce the number of reported issues");
-                EditorGUILayout.LabelField("- Use the Mute button to mark an issue as false-positive");
             }
         }
 
