fc6b3d6dfb7b29d49cb609da3b283c9027978803
Marco Trivellato <marcot@unity3d.com>
2021-04-22 11:36:04 +0100

    code formatting

--- Editor/Auditors/ScriptAuditor.cs
+++ Editor/Auditors/ScriptAuditor.cs
@@ -47,7 +47,7 @@ namespace Unity.ProjectAuditor.Editor.Auditors
                 new PropertyDefinition { type = PropertyType.Description, format = PropertyFormat.String, name = "Message", longName = "Compiler Message"},
                 new PropertyDefinition { type = PropertyType.Severity, name = "Type"},
                 new PropertyDefinition { type = PropertyType.Filename, name = "Filename", longName = "Filename and line number"},
-                new PropertyDefinition { type = PropertyType.Custom+1, format = PropertyFormat.String, name = "Target Assembly", longName = "Managed Assembly name" },
+                new PropertyDefinition { type = PropertyType.Custom + 1, format = PropertyFormat.String, name = "Target Assembly", longName = "Managed Assembly name" },
                 new PropertyDefinition { type = PropertyType.Path, name = "Full path"},
             }
         };
