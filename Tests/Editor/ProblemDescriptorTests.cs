3205cc5b446dde7d04a2432d9d490200032b55d0
Marco Trivellato <marcot@unity3d.com>
2021-04-30 14:58:23 +0100

    update formatting and api file

--- Tests/Editor/ProblemDescriptorTests.cs
+++ Tests/Editor/ProblemDescriptorTests.cs
@@ -122,11 +122,11 @@ namespace UnityEditor.ProjectAuditor.EditorTests
         public void ProblemDescriptorAnyPlatform()
         {
             var desc = new ProblemDescriptor
-            (
+                (
                 102001,
                 "test",
                 new[] {Area.CPU}
-            );
+                );
 
             Assert.True(ProblemDescriptorLoader.IsPlatformCompatible(desc));
         }
@@ -135,16 +135,16 @@ namespace UnityEditor.ProjectAuditor.EditorTests
         public void ProblemDescriptorSupportedPlatform()
         {
             var desc = new ProblemDescriptor
-            (
+                (
                 102001,
                 "test",
                 new[] {Area.CPU}
-            )
+                )
             {
 #if UNITY_EDITOR_WIN
-                platforms = new []{ BuildTarget.StandaloneWindows64.ToString() }
+                platforms = new[] { BuildTarget.StandaloneWindows64.ToString() }
 #else
-                platforms = new []{ BuildTarget.StandaloneOSX.ToString() }
+                platforms = new[] { BuildTarget.StandaloneOSX.ToString() }
 #endif
             };
 
@@ -155,13 +155,13 @@ namespace UnityEditor.ProjectAuditor.EditorTests
         public void ProblemDescriptorUnsupportedPlatform()
         {
             var desc = new ProblemDescriptor
-            (
+                (
                 102001,
                 "test",
                 new[] {Area.CPU}
-            )
+                )
             {
-                platforms = new []{ BuildTarget.Android.ToString() }  // assuming Android is not installed by default
+                platforms = new[] { BuildTarget.Android.ToString() }  // assuming Android is not installed by default
             };
 
             Assert.False(ProblemDescriptorLoader.IsPlatformCompatible(desc));
