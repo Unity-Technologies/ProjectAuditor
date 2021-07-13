938a0de12bcbad263cab457a83e113d3df308a90
Marco Trivellato <marcot@unity3d.com>
2021-07-13 21:32:18 +0100

    fix bad merge

--- Tests/Editor/AreaNameTests.cs
+++ Tests/Editor/AreaNameTests.cs
@@ -11,17 +11,19 @@ namespace UnityEditor.ProjectAuditor.EditorTests
         public void AreaNameIsValid()
         {
             // disabling stripUnusedMeshComponents will be reported as an issue
-            var stripUnusedMeshComponents = PlayerSettings.stripUnusedMeshComponents = false;
+            var stripUnusedMeshComponents = PlayerSettings.stripUnusedMeshComponents;
             PlayerSettings.stripUnusedMeshComponents = false;
 
             var issues = Utility.Analyze(IssueCategory.ProjectSetting);
 
             var issue = issues.FirstOrDefault(i => i.descriptor.method.Equals("stripUnusedMeshComponents"));
             Assert.NotNull(issue);
-            Assert.Contains(Area.BuildSize, issue.descriptor.areasAsEnums);
-            Assert.True(strippingIssue.descriptor.area.Contains(Area.LoadTimes.ToString()));
-            Assert.Contains(Area.GPU, issue.descriptor.areasAsEnums);
-            Assert.True(strippingIssue.descriptor.area.Equals("BuildSize|LoadTimes|GPU"));
+
+            var areas = issue.descriptor.areasAsEnums;
+            Assert.AreEqual(3, areas.Length);
+            Assert.Contains(Area.BuildSize, areas);
+            Assert.Contains(Area.GPU, areas);
+            Assert.Contains(Area.LoadTime, areas);
 
             // restore stripUnusedMeshComponents
             PlayerSettings.stripUnusedMeshComponents = stripUnusedMeshComponents;
