84b6f713fb14afe1d3414b3bb930477fae28df90
Marco Trivellato <marcot@unity3d.com>
2021-07-13 10:09:28 +0100

    make sure view selection and preferences persist

--- Editor/UI/BuildReportViewer.cs
+++ Editor/UI/BuildReportViewer.cs
@@ -1,3 +1,4 @@
+using System;
 using Editor.UI.Framework;
 using Unity.ProjectAuditor.Editor.Auditors;
 using Unity.ProjectAuditor.Editor.UI.Framework;
@@ -11,9 +12,44 @@ namespace Unity.ProjectAuditor.Editor.UI
     class BuildReportViewer : UnityEditor.Editor, IBuildReportProvider, IProjectIssueFilter
     {
         static int s_ActiveViewIndex;
+        static Preferences s_Preferences;
+
+        [SerializeField] int m_ActiveViewIndex;
+        [SerializeField] Preferences m_Preferences;
 
         ViewManager m_ViewManager;
-        Preferences m_Preferences;
+
+        void InitializeIfNeeded()
+        {
+            if (s_Preferences == null)
+            {
+                // static and non-static Preferences/ActiveViewIndex need to stay in sync so that they persists when user switches between reports
+                s_Preferences = m_Preferences = new Preferences();
+                s_ActiveViewIndex = m_ActiveViewIndex = 0;
+            }
+            else if (m_Preferences == null)
+            {
+                m_Preferences = s_Preferences;
+                m_ActiveViewIndex = s_ActiveViewIndex;
+            }
+
+            if (m_ViewManager == null)
+            {
+                var projectAuditor = new ProjectAuditor();
+                m_ViewManager = new ViewManager(new[] { IssueCategory.BuildStep, IssueCategory.BuildFile});
+                m_ViewManager.Create(projectAuditor, m_Preferences, this);
+                m_ViewManager.Audit(projectAuditor);
+                m_ViewManager.activeViewIndex = m_ActiveViewIndex;
+                m_ViewManager.onViewChanged = index => s_ActiveViewIndex = m_ActiveViewIndex = index;
+                (m_ViewManager.GetView(IssueCategory.BuildStep) as BuildReportView).buildReportProvider = this;
+                (m_ViewManager.GetView(IssueCategory.BuildFile) as BuildReportView).buildReportProvider = this;
+            }
+        }
+
+        void OnEnable()
+        {
+            InitializeIfNeeded();
+        }
 
         public override void OnInspectorGUI()
         {
@@ -23,18 +59,7 @@ namespace Unity.ProjectAuditor.Editor.UI
                 return;
             }
 
-            if (m_ViewManager == null)
-            {
-                var projectAuditor = new ProjectAuditor();
-                m_Preferences = new Preferences();
-                m_ViewManager = new ViewManager(new[] { IssueCategory.BuildStep, IssueCategory.BuildFile});
-                m_ViewManager.Create(projectAuditor, m_Preferences, this);
-                m_ViewManager.Audit(projectAuditor);
-                m_ViewManager.activeViewIndex = s_ActiveViewIndex;
-                m_ViewManager.onViewChanged = index => s_ActiveViewIndex = index;
-                (m_ViewManager.GetView(IssueCategory.BuildStep) as BuildReportView).buildReportProvider = this;
-                (m_ViewManager.GetView(IssueCategory.BuildFile) as BuildReportView).buildReportProvider = this;
-            }
+            InitializeIfNeeded();
 
             EditorGUILayout.BeginVertical(GUILayout.Height(Screen.height));
             var view = m_ViewManager.GetActiveView();
