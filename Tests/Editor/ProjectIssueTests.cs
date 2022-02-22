1478ed71a315c96f61c9524868dcadba523d5418
Marco Trivellato <marcot@unity3d.com>
2022-02-22 08:08:48 +0000

    better test coverage

--- Tests/Editor/ProjectIssueTests.cs
+++ Tests/Editor/ProjectIssueTests.cs
@@ -2,6 +2,7 @@ using System;
 using NUnit.Framework;
 using Unity.ProjectAuditor.Editor;
 using Unity.ProjectAuditor.Editor.CodeAnalysis;
+using Unity.ProjectAuditor.Editor.Utils;
 
 namespace Unity.ProjectAuditor.EditorTests
 {
@@ -44,6 +45,44 @@ namespace Unity.ProjectAuditor.EditorTests
             Assert.AreEqual(2, issue.GetNumCustomProperties());
             Assert.AreEqual(properties[0], issue.GetCustomProperty(0));
             Assert.AreEqual(properties[1], issue.GetCustomProperty(1));
+        }
+
+        [Test]
+        public void ProjectIssue_Properties_AreSet()
+        {
+            object[] properties =
+            {
+                "property #0",
+                "property #1"
+            };
+            var issue = new ProjectIssue(s_Descriptor, "dummy issue", IssueCategory.Code)
+            {
+                location = new Location("Assets/Dummy.cs")
+            };
+
+            issue.SetCustomProperties(properties);
+
+            Assert.AreEqual(2, issue.GetNumCustomProperties());
+            Assert.AreEqual("dummy issue", issue.GetProperty(PropertyType.Description));
+
+            Assert.AreEqual(Rule.Severity.Default.ToString(), issue.GetProperty(PropertyType.Severity));
+            Assert.AreEqual(Area.CPU.ToString(), issue.GetProperty(PropertyType.Area));
+            Assert.AreEqual("Assets/Dummy.cs:0", issue.GetProperty(PropertyType.Path));
+            Assert.AreEqual("Dummy.cs:0", issue.GetProperty(PropertyType.Filename));
+            Assert.AreEqual("cs", issue.GetProperty(PropertyType.FileType));
+            Assert.AreEqual(false.ToString(), issue.GetProperty(PropertyType.CriticalContext));
+            Assert.AreEqual(properties[0], issue.GetProperty(PropertyType.Num));
+            Assert.AreEqual(properties[1], issue.GetProperty(PropertyType.Num + 1));
+        }
+
+        [Test]
+        public void ProjectIssue_NoFileProperties_AreSet()
+        {
+            var issue = new ProjectIssue(s_Descriptor, "dummy issue", IssueCategory.Code);
+
+            Assert.AreEqual(ProjectIssueExtensions.k_NotAvailable, issue.GetProperty(PropertyType.Path));
+            Assert.AreEqual(ProjectIssueExtensions.k_NotAvailable, issue.GetProperty(PropertyType.Filename));
+            Assert.AreEqual(ProjectIssueExtensions.k_NotAvailable, issue.GetProperty(PropertyType.FileType));
         }
     }
 }
