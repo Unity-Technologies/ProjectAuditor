0163e1f93def40e47cd4adc4a57d435cedb4f92b
Marco Trivellato <marcot@unity3d.com>
2022-06-21 12:03:45 +0100

    minor update to usage instructions

--- Editor/UI/ShaderVariantsView.cs
+++ Editor/UI/ShaderVariantsView.cs
@@ -15,18 +15,18 @@ namespace Unity.ProjectAuditor.Editor.UI
 @"This view shows the built Shader Variants.
 
 To view the built Shader Variants, run your build pipeline and Refresh:
-- Build the project and/or Addressables/AssetBundles
-- Click the Refresh button
+* Build the project and/or Addressables/AssetBundles
+* Click the Refresh button
 Note that it's important to clear the cache before building Addressables.
 
 To clear the recorded variants use the Clear button";
 
         const string k_PlayerLogInstructions =
 @"The number of Variants contributes to the build size, however, there might be Variants that are not required (compiled) at runtime on the target platform. To find out which of these variants are not compiled at runtime, follow these steps:
-- Enable the Log Shader Compilation option
-- Make a Development build
-- Run the build on the target platform. Make sure to go through all scenes.
-- Drag & Drop the Player.log file on this window";
+* Enable the Log Shader Compilation option
+* Make a Development build
+* Run the build on the target platform. Make sure to go through all scenes.
+* Drag & Drop the Player.log file on this window";
 
         const string k_PlayerLogParsingDialogTitle = "Shader Variants";
 
