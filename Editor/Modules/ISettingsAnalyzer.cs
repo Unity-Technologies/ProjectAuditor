b6315e6848f6bb00321c0e725f71aded5c80281d
Marco Trivellato <marcot@unity3d.com>
2022-10-07 13:11:50 +0100

    move ISettingsAnalyzer to Modules

--- Editor/Modules/ISettingsAnalyzer.cs
+++ Editor/Modules/ISettingsAnalyzer.cs
@@ -1,8 +1,9 @@
 using System;
 using System.Collections.Generic;
+using Unity.ProjectAuditor.Editor.Core;
 using UnityEditor;
 
-namespace Unity.ProjectAuditor.Editor.Core
+namespace Unity.ProjectAuditor.Editor.Modules
 {
     public class SettingsAnalyzerContext
     {
