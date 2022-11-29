3c57e6a1561c1b4a7f397586733210ede0a1b3c8
Marco Trivellato <marcot@unity3d.com>
2022-11-29 09:17:52 +0000

    add PlayerSettings Mesh helpers

--- Editor/Utils/PlayerSettingsUtil.cs
+++ Editor/Utils/PlayerSettingsUtil.cs
@@ -6,6 +6,45 @@ namespace Unity.ProjectAuditor.Editor.Utils
 {
     public static class PlayerSettingsUtil
     {
+        internal static SerializedObject GetPlayerSettingsSerializedObject()
+        {
+            var internalGetPlayerSettingsMethod = typeof(PlayerSettings).GetMethod("InternalGetPlayerSettingsObject", BindingFlags.Static | BindingFlags.NonPublic);
+            if (internalGetPlayerSettingsMethod == null)
+                return null;
+
+            var playerSettings = internalGetPlayerSettingsMethod.Invoke(null, null);
+            if (playerSettings == null)
+                return null;
+
+            return new SerializedObject(playerSettings as UnityEngine.Object);
+        }
+
+        public static int GetVertexChannelCompressionMask()
+        {
+            var serializedSettings = GetPlayerSettingsSerializedObject();
+
+            var compressionFlagsProperty = serializedSettings.FindProperty("VertexChannelCompressionMask");
+            if (compressionFlagsProperty == null)
+                return 0;
+
+            return compressionFlagsProperty.intValue;
+        }
+
+        public static void SetVertexChannelCompressionMask(int newValue)
+        {
+            var serializedSettings = GetPlayerSettingsSerializedObject();
+
+            var compressionFlagsProperty = serializedSettings.FindProperty("VertexChannelCompressionMask");
+            if (compressionFlagsProperty == null)
+                return;
+
+            serializedSettings.Update();
+
+            compressionFlagsProperty.intValue = newValue;
+
+            serializedSettings.ApplyModifiedProperties();
+        }
+
         public static bool IsStaticBatchingEnabled(BuildTarget platform)
         {
             var method = typeof(PlayerSettings).GetMethod("GetBatchingForPlatform",
