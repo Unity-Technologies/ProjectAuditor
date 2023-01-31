1e00a5932625d83f7fe4dbe69441546e9878b2bd
Marco Trivellato <marcot@unity3d.com>
2023-01-31 09:31:22 +0000

    add GetGraphicsSettings wrapper support

--- Editor/Utils/AssemblyInfo.cs
+++ Editor/Utils/AssemblyInfo.cs
@@ -1,4 +1,6 @@
 using System;
 using System.Runtime.CompilerServices;
 
+[assembly: InternalsVisibleTo("Unity.ProjectAuditor.Editor")]
 [assembly: InternalsVisibleTo("Unity.ProjectAuditor.Editor.UI")]
+[assembly: InternalsVisibleTo("Unity.ProjectAuditor.EditorTests")]
