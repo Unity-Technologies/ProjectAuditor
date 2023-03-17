64219ce16119fb339452e64f8c546f585defa9c9
Steve McGreal <stephenm@unity3d.com>
2023-03-17 16:48:12 +0000

    Fixed borked tests merge

--- Tests/Editor/TextureTests.cs
+++ Tests/Editor/TextureTests.cs
@@ -1,15 +1,3 @@
-using System.Linq;
-using NUnit.Framework;
-using Unity.ProjectAuditor.Editor;
-using Unity.ProjectAuditor.Editor.Modules;
-using Unity.ProjectAuditor.Editor.Tests.Common;
-using UnityEditor;
-using UnityEngine;
-using UnityEngine.Profiling;
-
-namespace Unity.ProjectAuditor.EditorTests
-{
-    class TextureTests : TestFixtureBase
     {
         const string k_TextureName = "ProceduralTextureForTest3212";
         const string k_TextureNameMipMapDefault = k_TextureName + "MipMapDefaultTest1234";
@@ -118,17 +106,25 @@ namespace Unity.ProjectAuditor.EditorTests
             textureImporter.filterMode = FilterMode.Bilinear;
             textureImporter.mipmapEnabled = true;
             textureImporter.SaveAndReimport();
-            
-            m_TextureNameSolidColor = new TestAsset(k_TextureNameSolidColor + ".png", encodedPNG);
+
+            var solidColorTexture = new Texture2D(2, 2, TextureFormat.RGBA32, false);
+            solidColorTexture.SetPixel(0, 0, Color.red);
+            solidColorTexture.SetPixel(1, 0, Color.red);
+            solidColorTexture.SetPixel(0, 1, Color.red);
+            solidColorTexture.SetPixel(1, 1, Color.red);
+
+            var encodedSolidColorPNG = solidColorTexture.EncodeToPNG();
+            m_TextureNameSolidColor = new TestAsset(k_TextureNameSolidColor + ".png", encodedSolidColorPNG);
+            textureImporter = AssetImporter.GetAtPath(m_TextureNameSolidColor.relativePath) as TextureImporter;
             textureImporter.SaveAndReimport();
 
             var notSolidColorTexture = new Texture2D(2, 2, TextureFormat.RGBA32, false);
-            largeTexture.SetPixel(0, 0, Color.blue);
-            largeTexture.SetPixel(1, 0, Color.red);
+            notSolidColorTexture.SetPixel(0, 0, Color.blue);
+            notSolidColorTexture.SetPixel(1, 0, Color.red);
 
             var encodedNotSolidColorPNG = notSolidColorTexture.EncodeToPNG();
             m_TextureNameNotSolidColor = new TestAsset(k_TextureNameNotSolidColor + ".png", encodedNotSolidColorPNG);
-            textureImporter = AssetImporter.GetAtPath(m_TextureNameStreamingMipmapEnabled.relativePath) as TextureImporter;
+            textureImporter = AssetImporter.GetAtPath(m_TextureNameNotSolidColor.relativePath) as TextureImporter;
             textureImporter.SaveAndReimport();
         }
 
@@ -301,21 +297,20 @@ namespace Unity.ProjectAuditor.EditorTests
             textureImporter.filterMode = FilterMode.Bilinear;
             textureImporter.SaveAndReimport();
         }
-  
+
         [Test]
         public void Texture_SolidTexture_IsReported()
         {
-            var textureDiagnostic = AnalyzeAndFindAssetIssues(m_TextureNameNotSolidColor, IssueCategory.AssetDiagnostic).FirstOrDefault(i => i.descriptor.Equals(TextureAnalyzer.k_TextureSolidColorDescriptor));
+            var textureDiagnostic = AnalyzeAndFindAssetIssues(m_TextureNameSolidColor, IssueCategory.AssetDiagnostic).FirstOrDefault(i => i.descriptor.Equals(TextureAnalyzer.k_TextureSolidColorDescriptor));
 
             Assert.IsNotNull(textureDiagnostic);
         }
 
         [Test]
-        public void Texture_SolidTexture_IsNotReported()
+        public void Texture_Not_SolidTexture_IsNotReported()
         {
-            var textureDiagnostic = AnalyzeAndFindAssetIssues(m_TextureNameSolidColor, IssueCategory.AssetDiagnostic).FirstOrDefault(i => i.descriptor.Equals(TextureAnalyzer.k_TextureSolidColorDescriptor));
+            var textureDiagnostic = AnalyzeAndFindAssetIssues(m_TextureNameNotSolidColor, IssueCategory.AssetDiagnostic).FirstOrDefault(i => i.descriptor.Equals(TextureAnalyzer.k_TextureSolidColorDescriptor));
 
             Assert.IsNull(textureDiagnostic);
         }
     }
-}
