dda96078df0727d4c50b263ec01f6c52445e29df
Steve McGreal <stephenm@unity3d.com>
2023-08-08 15:42:29 +0100

    Expose ProjectAuditorSettings and (most of) ProjectAuditorConfig
    These ScriptableObjects should be exposed to the Inspector so users can view and edit them as required. All exposed types, enums and variables already have properly-formatted comments for generating correct API documentation.

--- Editor/ProjectAuditorSettings.cs
+++ Editor/ProjectAuditorSettings.cs
@@ -11,51 +11,51 @@ namespace Unity.ProjectAuditor.Editor
         /// <summary>
         /// Maximum mesh vertex count. If we find meshes with more triangles we report an issue.
         /// </summary>
-        internal int MeshVerticeCountLimit = 5000;
+        public int MeshVerticeCountLimit = 5000;
 
         /// <summary>
         /// Maximum mesh vertex count. If we find meshes with more triangles we report an issue.
         /// </summary>
-        internal int MeshTriangleCountLimit = 5000;
+        public int MeshTriangleCountLimit = 5000;
 
         /// <summary>
         /// Maximum texture size. If we find textures at this size we warn the user. If they exceed the size we report a critical issue.
         /// </summary>
-        internal int TextureSizeLimit = 2048;
+        public int TextureSizeLimit = 2048;
 
         /// <summary>
         /// Maximum size of all files in the StreamingAssets folder. Beyond that size we report an issue.
         /// </summary>
-        internal int StreamingAssetsFolderSizeLimit = 50;
+        public int StreamingAssetsFolderSizeLimit = 50;
 
         /// <summary>
         /// Maximum size when it is not necessary to enable Streaming Mipmaps. Beyond that size we report an issue.
         /// </summary>
-        internal int TextureStreamingMipmapsSizeLimit = 4000;
+        public int TextureStreamingMipmapsSizeLimit = 4000;
 
         /// <summary>
         /// Percent of empty space allowed in a Sprite Atlas texture. Beyond that size we report an issue.
         /// </summary>
-        internal int SpriteAtlasEmptySpaceLimit = 50;
+        public int SpriteAtlasEmptySpaceLimit = 50;
 
         /// <summary>
         /// The runtime size (in bytes) above which we report an issue for non-streaming AudioClips. Unity's AudioClip streaming buffers are 200KB in size.
         /// </summary>
-        internal int StreamingClipThresholdBytes = 200 * 1024;
+        public int StreamingClipThresholdBytes = 200 * 1024;
 
         /// <summary>
         /// The runtime size (in bytes) above which we report an issue for AudioClips that are set to Decompress On Load.
         /// </summary>
-        internal int LongDecompressedClipThresholdBytes = 200 * 1024;
+        public int LongDecompressedClipThresholdBytes = 200 * 1024;
 
         /// <summary>
         /// The file size (in bytes) above which we report advice on reducing the size of compressed AudioClips on mobile.
         /// </summary>
-        internal int LongCompressedMobileClipThresholdBytes = 200 * 1024;
+        public int LongCompressedMobileClipThresholdBytes = 200 * 1024;
 
         /// <summary>
         /// The file size (in bytes) above which we recommend that AudioClips be flagged to load in the background.
         /// </summary>
-        internal int LoadInBackGroundClipSizeThresholdBytes = 200 * 1024;
+        public int LoadInBackGroundClipSizeThresholdBytes = 200 * 1024;
     }
 }
