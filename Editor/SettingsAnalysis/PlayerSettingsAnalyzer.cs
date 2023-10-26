78e5fe53c13582522818e453295a7fd75dd6f859
Steve McGreal <stephenm@unity3d.com>
2023-10-26 15:03:24 +0100

    Replaced '➔' characters in strings with '>'

--- Editor/SettingsAnalysis/PlayerSettingsAnalyzer.cs
+++ Editor/SettingsAnalysis/PlayerSettingsAnalyzer.cs
@@ -38,14 +38,14 @@ namespace Unity.ProjectAuditor.Editor.SettingsAnalysis
             "Player: Splash Screen is enabled",
             new[] { Area.LoadTime },
             "<b>Show Splash Screen</b> is enabled in the Player Settings. Displaying a splash screen will increase the time it takes to load into the first scene.",
-            "Disable the Splash Screen option in <b>Player Settings ➔ Splash Image ➔ Show Splash Screen</b>.");
+            "Disable the Splash Screen option in <b>Project Settings > Player > Splash Image > Show Splash Screen</b>.");
 
         static readonly Descriptor k_SpeakerModeDescriptor = new Descriptor(
             PAS0033,
             "Audio: Speaker Mode is not set to Mono",
             new[] { Area.BuildSize, Area.Memory },
             "<b>Default Speaker Mode</b> in Audio Settings is not set to <b>Mono</b>. This may result in a build which is larger than necessary and which occupies more audio memory at runtime. Many mobile devices have limited or nonexistent stereo speaker options.",
-            "Change <b>Audio Settings ➔ Default Speaker Mode</b> to <b>Mono</b>. You should also consider enabling the <b>Force To Mono</b> AudioClip import setting to reduce import times and build size.")
+            "Change <b>Project Settings > Audio > Default Speaker Mode</b> to <b>Mono</b>. You should also consider enabling the <b>Force To Mono</b> AudioClip import setting to reduce import times and build size.")
         {
             platforms = new[] { "Android", "iOS"},
             fixer = (issue =>
@@ -59,7 +59,7 @@ namespace Unity.ProjectAuditor.Editor.SettingsAnalysis
             "Player: IL2CPP Compiler Configuration is set to Master",
             new[] { Area.BuildTime },
             "<b>C++ Compiler Configuration</b> in Player Settings is set to <b>Master</b>. This mode is intended for shipping builds and will significantly increase build times.",
-            "Change <b>Player Settings ➔ Other Settings ➔ Configuration ➔ C++ Compiler Configuration</b> to <b>Release</b>.")
+            "Change <b>Project Settings > Player > Other Settings > Configuration > C++ Compiler Configuration</b> to <b>Release</b>.")
         {
             fixer = (issue =>
             {
@@ -75,7 +75,7 @@ namespace Unity.ProjectAuditor.Editor.SettingsAnalysis
             "Player: IL2CPP Compiler Configuration is set to Debug",
             new[] { Area.CPU },
             "<b>C++ Compiler Configuration</b> is set to <b>Debug</b>. This mode is intended for debugging and might have an impact on runtime CPU performance.",
-            "Change <b>Player Settings ➔ Other Settings ➔ Configuration ➔ C++ Compiler Configuration</b> to <b>Release</b>.")
+            "Change <b>Project Settings > Player > Other Settings > Configuration > C++ Compiler Configuration</b> to <b>Release</b>.")
         {
             fixer = (issue =>
             {
@@ -91,7 +91,7 @@ namespace Unity.ProjectAuditor.Editor.SettingsAnalysis
             "Player: Lightmap Streaming is disabled",
             new[] { Area.GPU, Area.CPU },
             "<b>Lightmap Streaming</b> in Player Settings is not enabled. As a result, all lightmap detail levels are loaded into GPU memory, potentially resulting in excessive lightmap texture memory usage.",
-            "Enable <b>Lightmap Streaming</b> in <b>Player Settings ➔ Other Settings ➔ Rendering</b>.")
+            "Enable <b>Lightmap Streaming</b> in <b>PProject Settings > Player > Other Settings > Rendering</b>.")
         {
             fixer = (issue =>
             {
