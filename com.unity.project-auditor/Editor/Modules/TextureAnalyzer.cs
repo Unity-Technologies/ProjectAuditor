7d7e24aeb2dc37f12bc9bd82ce7e54b7c2038be7
Marco Trivellato <marcot@unity3d.com>
2024-01-04 12:47:14 +0000

    fix compilation

--- com.unity.project-auditor/Editor/Modules/TextureAnalyzer.cs
+++ com.unity.project-auditor/Editor/Modules/TextureAnalyzer.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using Unity.ProjectAuditor.Editor.Core;
 using Unity.ProjectAuditor.Editor.Diagnostic;
 using Unity.ProjectAuditor.Editor.Interfaces;
+using Unity.ProjectAuditor.Editor.Utils;
 using UnityEditor;
 using UnityEngine;
 using UnityEngine.Profiling;
