34ad348cf6ba6e77e02ba5d3d7a1bd5adb453ffe
Marco Trivellato <marcot@unity3d.com>
2024-02-06 14:51:59 +0000

    auto code formatting

--- com.unity.project-auditor/Editor/UI/ProjectAuditorWindow.cs
+++ com.unity.project-auditor/Editor/UI/ProjectAuditorWindow.cs
@@ -924,7 +924,7 @@ namespace Unity.ProjectAuditor.Editor.UI
             m_AnalysisState = AnalysisState.InProgress;
             m_Report = null;
 
-            if(m_ProjectAuditor == null)
+            if (m_ProjectAuditor == null)
                 m_ProjectAuditor = new ProjectAuditor();
 
             var analysisParams = new AnalysisParams
@@ -973,7 +973,7 @@ namespace Unity.ProjectAuditor.Editor.UI
 
         internal void AuditCategories(IssueCategory[] categories)
         {
-            if(m_ProjectAuditor == null)
+            if (m_ProjectAuditor == null)
                 m_ProjectAuditor = new ProjectAuditor();
 
             // a module might report more categories than requested so we need to make sure we clean up the views accordingly
@@ -1562,6 +1562,7 @@ namespace Unity.ProjectAuditor.Editor.UI
                 }
             }
         }
+
 #endif
 
         void DrawReport()
@@ -1917,7 +1918,7 @@ namespace Unity.ProjectAuditor.Editor.UI
                     return;
                 }
 
-                if(m_ProjectAuditor == null)
+                if (m_ProjectAuditor == null)
                     m_ProjectAuditor = new ProjectAuditor();
 
                 m_LoadButtonAnalytic =  AnalyticsReporter.BeginAnalytic();
